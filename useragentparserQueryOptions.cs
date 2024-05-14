216b7f88bc09873020c1aba59309c9e544b2fd08
APIVerve Team <hello@apiverve.com>
2024-05-14 15:44:29 -0500

    Bug fixes and Improvements

---
+++ useragentparserQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class useragentparserQueryOptions {
+public string ua { get; set; }
+}
+
+}
