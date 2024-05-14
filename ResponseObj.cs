8c9bf61b5c90e96b94eccacbcf054296e2d2a9cd
APIVerve Team <hello@apiverve.com>
2024-05-14 15:44:26 -0500

    Bug fixes and Improvements

--- ResponseObj.cs
+++ ResponseObj.cs
@@ -1,21 +1,82 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
+using Newtonsoft.Json;
 
 namespace APIVerve
 {
-    using Newtonsoft.Json;
+public class browser
+{
+    [JsonProperty("name")]
+    public string name { get; set; }
+
+    [JsonProperty("version")]
+    public string version { get; set; }
+
+    [JsonProperty("major")]
+    public string major { get; set; }
+
+}
+
+public class engine
+{
+    [JsonProperty("name")]
+    public string name { get; set; }
+
+    [JsonProperty("version")]
+    public string version { get; set; }
+
+}
+
+public class os
+{
+    [JsonProperty("name")]
+    public string name { get; set; }
+
+    [JsonProperty("version")]
+    public string version { get; set; }
+
+}
+
+public class device
+{
+}
+
+public class cpu
+{
+}
+
+public class data
+{
+    [JsonProperty("ua")]
+    public string ua { get; set; }
+
+    [JsonProperty("browser")]
+    public browser browser { get; set; }
+
+    [JsonProperty("engine")]
+    public engine engine { get; set; }
+
+    [JsonProperty("os")]
+    public os os { get; set; }
+
+    [JsonProperty("device")]
+    public device device { get; set; }
+
+    [JsonProperty("cpu")]
+    public cpu cpu { get; set; }
+
+}
 
 public class ResponseObj
 {
     [JsonProperty("status")]
-    public string Status { get; set; }
+    public string status { get; set; }
 
     [JsonProperty("error")]
-    public string Error { get; set; }
+    public object error { get; set; }
 
     [JsonProperty("data")]
-    public object Data { get; set; }
+    public data data { get; set; }
 
 }
 
